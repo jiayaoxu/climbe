6dccdc0e11e91457943922efb0acbff6100b765d
shine <15065571038@163.com>
2017-10-28 20:39:54 +0800

    海选ok

---
+++ src/main/java/cn/newcode/climb/po/rank.java
@@ -0,0 +1,28 @@
+package cn.newcode.climb.po;
+
+/**
+ * @Description: java类作用描述
+ * @author: 作者姓名
+ * @CreateDate: 2017/10/28 20:17
+ * @Version: 1.0
+ */
+public class rank {
+    private Integer rank;
+    private Integer mid;
+
+    public Integer getRank() {
+        return rank;
+    }
+
+    public void setRank(Integer rank) {
+        this.rank = rank;
+    }
+
+    public Integer getMid() {
+        return mid;
+    }
+
+    public void setMid(Integer mid) {
+        this.mid = mid;
+    }
+}
