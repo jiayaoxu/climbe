a4b5f44c5234de0108cf53cf0ea918a1e79bc34f
shine <15065571038@163.com>
2017-10-29 10:41:25 +0800

    对战

---
+++ src/main/java/cn/newcode/climb/po/hasIn.java
@@ -0,0 +1,28 @@
+package cn.newcode.climb.po;
+
+/**
+ * @Description: 判断是否进入下一场赛事
+ * @author: shine
+ * @CreateDate: 2017/10/28 19:31
+ * @Version: 1.0
+ */
+public class hasIn {
+    private Boolean isIN;
+    private Integer total;
+
+    public Boolean getIN() {
+        return isIN;
+    }
+
+    public void setIN(Boolean IN) {
+        isIN = IN;
+    }
+
+    public Integer getTotal() {
+        return total;
+    }
+
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
+}
