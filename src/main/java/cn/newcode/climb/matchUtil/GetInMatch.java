eacb6650bda4aa7bf17a201e25ae9b0766263dc5
shine <15065571038@163.com>
2017-10-31 18:53:19 +0800

    获取比赛的信息

---
+++ src/main/java/cn/newcode/climb/matchUtil/GetInMatch.java
@@ -0,0 +1,28 @@
+package cn.newcode.climb.matchUtil;
+
+/**
+ * @Description: java类作用描述
+ * @author: 作者姓名
+ * @CreateDate: 2017/10/31 11:27
+ * @Version: 1.0
+ */
+public class GetInMatch {
+    private Integer mid;
+    private Integer total;
+
+    public Integer getMid() {
+        return mid;
+    }
+
+    public void setMid(Integer mid) {
+        this.mid = mid;
+    }
+
+    public Integer getTotal() {
+        return total;
+    }
+
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
+}
