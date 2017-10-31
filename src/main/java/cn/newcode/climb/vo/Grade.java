ff849a083e4c6a23e419a495901ad59f1cb036a3
shine <15065571038@163.com>
2017-10-31 18:52:52 +0800

    返回成绩vo

--- src/main/java/cn/newcode/climb/vo/Grade.java
+++ src/main/java/cn/newcode/climb/vo/Grade.java
@@ -7,10 +7,30 @@ package cn.newcode.climb.vo;
  * @Version: 1.0
  */
 public class Grade {
+    //排名
     private Integer ranking;
-
+    //是否晋级
     private Boolean hasNext;
+    //总晋级人数
+    private Integer totalIn;
+    //对手id
+    private Integer equal;
 
+    public Integer getEqual() {
+        return equal;
+    }
+
+    public void setEqual(Integer equal) {
+        this.equal = equal;
+    }
+
+    public Integer getTotalIn() {
+        return totalIn;
+    }
+
+    public void setTotalIn(Integer totalIn) {
+        this.totalIn = totalIn;
+    }
 
     public Integer getRanking() {
         return ranking;
