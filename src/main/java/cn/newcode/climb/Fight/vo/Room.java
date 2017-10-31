d3d1c1e0f6a636d77cd3f03681da686b678083e8
shine <15065571038@163.com>
2017-10-31 18:50:22 +0800

    房间

---
+++ src/main/java/cn/newcode/climb/Fight/vo/Room.java
@@ -0,0 +1,19 @@
+package cn.newcode.climb.Fight.vo;
+
+/**
+ * @Description: java类作用描述
+ * @author: 作者姓名
+ * @CreateDate: 2017/10/29 16:52
+ * @Version: 1.0
+ */
+public class Room {
+    private Integer rid;
+
+    public Integer getRid() {
+        return rid;
+    }
+
+    public void setRid(Integer rid) {
+        this.rid = rid;
+    }
+}
