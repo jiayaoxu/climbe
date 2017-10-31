8d463f4eb0802b451d65ad081abc65db2481996b
shine <15065571038@163.com>
2017-10-31 18:52:19 +0800

    用户管理

--- src/main/java/cn/newcode/climb/Fight/tool/UserManager.java
+++ src/main/java/cn/newcode/climb/Fight/tool/UserManager.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * @Description: 放用户的socket
+ * @Description: 存放用户的socket
  * @author: shine
  * @CreateDate: 2017/10/27 16:30
  * @Version: 1.0
@@ -31,26 +31,50 @@ public class UserManager {
 
    private UserManager(){}
 
+   //实例化
    public static UserManager getInstance(){
       return Instance.userManager;
    }
 
+   //添加上线玩家
    public void addPlayer(Integer uid,Socket socket){
       userMap.put(uid,socket);
    }
 
+   //玩家下线
    public void removePlayer(Integer uid){
       userMap.remove(uid);
    }
 
+   //获取玩家信息
+   public Socket getPlayer(Integer uid){
+      return userMap.get(uid);
+   }
+
+   //获取用户列表
+   public Map<Integer, Socket> getUserMap() {
+      return userMap;
+   }
+
+   //创建房间
    public void createRoom(Integer uid){
       List<Integer> roomPlayers = new ArrayList<Integer>();
       roomPlayers.add(uid);
       roomMap.put(uid,roomPlayers);
    }
 
+   //玩家加入房间
+   public void playerJoinRoom(Integer rid,List<Integer> players){
+      roomMap.put(rid,players);
+   }
+
+   //获取用户房间信息
    public List<Integer> getRoomMap(Integer rid){
       return roomMap.get(rid);
    }
 
+   //获取所有房间
+   public Map<Integer, List<Integer>> getRoomList() {
+      return roomMap;
+   }
 }
