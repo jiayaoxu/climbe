22469cf479d4546dfe75c2c602608280d86692de
shine <15065571038@163.com>
2017-11-19 13:43:42 +0800

    处理对战日志

---
+++ src/main/java/cn/newcode/climb/Fight/tool/MLogger.java
@@ -0,0 +1,18 @@
+package cn.newcode.climb.Fight.tool;
+
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+public class MLogger {
+	public static Logger log=(Logger) LogManager.getLogger(MLogger.class);
+	public static void info(String msg) {
+		log.info(msg);
+	}
+	public static void warn(String msg) {
+		log.warn(msg);
+	}
+	public static void error(Throwable e) {
+		log.error("", e);
+	}
+}
