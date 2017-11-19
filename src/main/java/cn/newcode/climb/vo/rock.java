a57d7f9edad014c7a8d8efd348bedd0d0781b816
shine <15065571038@163.com>
2017-11-19 13:45:19 +0800

    处理定线vo

---
+++ src/main/java/cn/newcode/climb/vo/rock.java
@@ -0,0 +1,25 @@
+package cn.newcode.climb.vo;
+
+import cn.newcode.climb.po.Rock;
+import cn.newcode.climb.po.RockPoint;
+
+import java.util.List;
+
+/**
+ * @Description: java类作用描述
+ * @author: 作者姓名
+ * @CreateDate: 2017/11/19 3:35
+ * @Version: 1.0
+ */
+public class rock {
+
+    private List<RockPoint> yanshi;
+
+    public List<RockPoint> getYanshi() {
+        return yanshi;
+    }
+
+    public void setYanshi(List<RockPoint> yanshi) {
+        this.yanshi = yanshi;
+    }
+}
