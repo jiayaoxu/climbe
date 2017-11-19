d6e5e512b970104f52bfd081a69545654053245a
shine <15065571038@163.com>
2017-11-19 13:44:53 +0800

    处理定线service

---
+++ src/main/java/cn/newcode/climb/service/RockService.java
@@ -0,0 +1,61 @@
+package cn.newcode.climb.service;
+
+import cn.newcode.climb.po.Rock;
+import cn.newcode.climb.po.RockHall;
+import cn.newcode.climb.po.RockPoint;
+import cn.newcode.climb.po.RockWall;
+
+import java.util.List;
+
+/**
+ * @Description: 定线service
+ * @author: shine
+ * @CreateDate: 2017/11/18 22:58
+ * @Version: 1.0
+ */
+public interface RockService {
+
+    /**
+     * 列出所有的岩馆
+     */
+    List<RockHall> ListHalls() throws Exception;
+
+    /**
+     * 通过岩馆id查找岩馆下所有岩壁
+     * @param hid
+     * @return
+     * @throws Exception
+     */
+    List<RockWall> ListWalls(Integer hid) throws Exception;
+
+    /**
+     * 在指定岩馆上创建岩壁,返回岩壁id
+     * @param wall
+     * @return
+     * @throws Exception
+     */
+    Integer createWall(RockWall wall) throws Exception;
+
+    /**
+     * 在指定岩壁上添加岩点
+     * @param points
+     * @return
+     * @throws Exception
+     */
+    Integer addPoint(List<RockPoint> points) throws Exception;
+
+    /**
+     * 通过指定岩壁查询岩点
+     * @param wid
+     * @return
+     * @throws Exception
+     */
+    List<RockPoint> selectPoint(Integer wid) throws Exception;
+
+    /**
+     * 列出所有岩石
+     * @return
+     * @throws Exception
+     */
+    List<Rock> selectRocks() throws Exception;
+}
---
+++ src/main/java/cn/newcode/climb/service/impl/RockServiceImpl.java
@@ -0,0 +1,70 @@
+package cn.newcode.climb.service.impl;
+
+import cn.newcode.climb.mapper.RockHallMapper;
+import cn.newcode.climb.mapper.RockMapper;
+import cn.newcode.climb.mapper.RockPointMapper;
+import cn.newcode.climb.mapper.RockWallMapper;
+import cn.newcode.climb.po.Rock;
+import cn.newcode.climb.po.RockHall;
+import cn.newcode.climb.po.RockPoint;
+import cn.newcode.climb.po.RockWall;
+import cn.newcode.climb.service.RockService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @Description: 定线Service
+ * @author: shine
+ * @CreateDate: 2017/11/18 22:58
+ * @Version: 1.0
+ */
+@Service("rockService")
+public class RockServiceImpl implements RockService {
+
+    @Autowired
+    private RockHallMapper rockHallMapper;
+
+    @Autowired
+    private RockWallMapper rockWallMapper;
+
+    @Autowired
+    private RockPointMapper rockPointMapper;
+
+    @Autowired
+    private RockMapper rockMapper;
+
+    @Override
+    public List<RockHall> ListHalls() throws Exception {
+        return rockHallMapper.selectHalls();
+    }
+
+    @Override
+    public List<RockWall> ListWalls(Integer hid) throws Exception {
+        return rockWallMapper.selectWalls(hid);
+    }
+
+    @Override
+    public Integer createWall(RockWall wall) throws Exception {
+        return rockWallMapper.insertSelective(wall);
+    }
+
+    @Override
+    public Integer addPoint(List<RockPoint> points) throws Exception {
+        for(RockPoint p : points){
+            rockPointMapper.insertSelective(p);
+        }
+        return 1;
+    }
+
+    @Override
+    public List<RockPoint> selectPoint(Integer wid) throws Exception {
+        return rockPointMapper.selectPoints(wid);
+    }
+
+    @Override
+    public List<Rock> selectRocks() throws Exception {
+        return rockMapper.selectRocks();
+    }
+}
