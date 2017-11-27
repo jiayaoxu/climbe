a24d5e4c3672a132759e4a73fbe3936a4e5e5eb9
shine <15065571038@163.com>
2017-11-27 23:25:45 +0800

    对战、比赛基本完善

---
+++ src/test/java/testMap.java
@@ -0,0 +1,73 @@
+import cn.newcode.climb.po.Match;
+import cn.newcode.climb.po.User;
+import cn.newcode.climb.service.MatchService;
+import cn.newcode.climb.service.impl.MatchServiceImpl;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Description: map测试
+ * @author: shine
+ * @CreateDate: 2017/10/31 13:30
+ * @Version: 1.0
+ */
+public class testMap {
+
+    @Test
+    public void testMap() {
+        Map map = new HashMap();
+        map.put(1,1);
+        Integer a = (Integer) map.get(2);
+        System.out.println(a);
+    }
+
+    @Test
+    public void testList(){
+        List list = new ArrayList();
+        list.add(1);
+        System.out.println(list.get(0));
+    }
+
+    @Test
+    public void test(){
+        MatchServiceImpl matchService = new MatchServiceImpl();
+        try{
+            List<Match> m = matchService.selectMatchs(true);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    public void testLists(){
+        List<User> list = new ArrayList<User>();
+        User user = new User();
+        user.setUsername("123");
+        user.setPassword("123");
+        User user1 = new User();
+        user1.setUsername("321");
+        user1.setPassword("321");
+        list.add(user);
+        list.add(user1);
+        for(int i = 0;i<list.size();i++){
+            System.out.println("用户名:"+list.get(i).getUsername()+"密码:"+list.get(i).getPassword());
+        }
+    }
+
+    @Test
+    public void fun(){
+        String str = "123@123@123";
+        String []strs = str.split("@");
+        for(String s : strs){
+            System.out.println(s);
+        }
+    }
+
+
+}
